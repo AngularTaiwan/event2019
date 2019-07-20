f7c60afbc8911d7ceb9dbf6ff1fbb743ced918ba
Kevin Yang <chgc_kevin@hotmail.com>
2019-07-20 22:00:24 +0800

    加入 COC 頁面

--- web/projects/event2019/src/app/app.module.ts
+++ web/projects/event2019/src/app/app.module.ts
@@ -1,19 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { CocComponent } from './coc/coc.component';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  { path: 'coc', component: CocComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CocComponent, MainComponent],
   imports: [
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ web/projects/event2019/src/app/coc/coc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coc',
+  templateUrl: './coc.component.html',
+  styleUrls: ['./coc.component.css']
+})
+export class CocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
