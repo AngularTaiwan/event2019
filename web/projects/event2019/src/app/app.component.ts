22f6efaec09a60373800895ce60c1d96028d0227
Kevin Yang <chgc_kevin@hotmail.com>
2019-07-22 10:25:00 +0800

    新增 goTop 功能

--- web/projects/event2019/src/app/app.component.ts
+++ web/projects/event2019/src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  HostListener,
+  Inject,
+  ChangeDetectorRef
+} from '@angular/core';
 import { CheckForUpdateService } from './services/check-for-update.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +15,46 @@ import { CheckForUpdateService } from './services/check-for-update.service';
 })
 export class AppComponent implements OnDestroy {
   title = 'event2019';
+  windowScrolled: boolean;
+  private prevWindowScrolled: boolean;
 
-  constructor(private check: CheckForUpdateService) {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (
+      window.pageYOffset ||
+      this.document.documentElement.scrollTop ||
+      this.document.body.scrollTop > 100
+    ) {
+      this.windowScrolled = true;
+    } else if (
+      (this.windowScrolled && window.pageYOffset) ||
+      this.document.documentElement.scrollTop ||
+      this.document.body.scrollTop < 10
+    ) {
+      this.windowScrolled = false;
+    }
+    if (this.windowScrolled !== this.prevWindowScrolled) {
+      this.prevWindowScrolled = this.windowScrolled;
+      this.cd.detectChanges();
+    }
+  }
+  constructor(
+    private check: CheckForUpdateService,
+    @Inject(DOCUMENT) private document: Document,
+    private cd: ChangeDetectorRef
+  ) {
     check.registe();
+  }
+
+  scrollToTop() {
+    (function smoothscroll(prevScroll) {
+      const currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0 && currentScroll <= prevScroll) {
+        window.requestAnimationFrame(smoothscroll.bind(null, currentScroll));
+        window.scrollTo(0, currentScroll - currentScroll / 12);
+      }
+    })(document.documentElement.scrollTop || document.body.scrollTop);
   }
 
   ngOnDestroy() {
--- web/projects/event2019/src/app/services/check-for-update.service.ts
+++ web/projects/event2019/src/app/services/check-for-update.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ApplicationRef } from '@angular/core';
+import { Injectable, ApplicationRef, Optional } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { interval, concat, Subject } from 'rxjs';
 import { first, takeUntil } from 'rxjs/operators';
@@ -9,19 +9,25 @@ import { first, takeUntil } from 'rxjs/operators';
 export class CheckForUpdateService {
   destory$ = new Subject();
 
-  constructor(private appRef: ApplicationRef, private updates: SwUpdate) {
-  }
+  constructor(
+    private appRef: ApplicationRef,
+    @Optional() private updates: SwUpdate
+  ) {}
 
   registe() {
     // Allow the app to stabilize first, before starting polling for updates with `interval()`.
-    const appIsStable$ = this.appRef.isStable.pipe(first(isStable => isStable === true));
-    const everySixHours$ = interval(6 * 60 * 60 * 1000);
-    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$).pipe(
-      takeUntil(this.destory$),
+    const appIsStable$ = this.appRef.isStable.pipe(
+      first(isStable => isStable === true)
     );
-
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(
+      appIsStable$,
+      everySixHours$
+    ).pipe(takeUntil(this.destory$));
     everySixHoursOnceAppIsStable$.subscribe(() => {
-      this.updates.checkForUpdate();
+      if (this.updates.isEnabled) {
+        this.updates.checkForUpdate();
+      }
     });
 
     this.updateListener();
