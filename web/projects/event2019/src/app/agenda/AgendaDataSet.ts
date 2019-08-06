e50f9a7ea68942239b36b8eb67b0dc8830cfe24e
Kevin Yang <chgc_kevin@hotmail.com>
2019-08-06 14:43:26 +0800

    temp agenda

--- web/projects/event2019/src/app/agenda/AgendaDataSet.ts
+++ web/projects/event2019/src/app/agenda/AgendaDataSet.ts
@@ -69,17 +69,18 @@ export const AgendaDataSet = {
       isSession: true,
       startTime: '10:00',
       endTime: '11:00',
-      title: 'TBD',
+      title: 'Lighthouse - Performance Tuning',
       description: '',
-      speaker: 'TBD'
+      speaker: 'Paul Li'
     },
     {
       isSession: true,
       startTime: '11:00',
       endTime: '12:00',
-      title: 'TBD',
-      description: '',
-      speaker: 'TBD'
+      title: 'Bazel',
+      description:
+        'Use Bazel to build your Angular Application - Bazel is an amazing build tool used in all products inside Google, and in the future version of Angular, bazel will also become the default build tool. Bazel is a fast/reliable/extensible build system with a lot of amazing features. In this talk we will introduce the basic concepts of bazel and also provide a demo to talk about how to build/test/bundle Angular application with bazel.',
+      speaker: '李嘉'
     },
     {
       isSession: false,
@@ -91,9 +92,10 @@ export const AgendaDataSet = {
       isSession: true,
       startTime: '13:00',
       endTime: '14:00',
-      title: 'TBD',
-      description: '',
-      speaker: 'TBD'
+      title: 'The hidden docs of Angular',
+      description:
+        '介紹一些有用但是 Angular 文檔上幾乎沒有涉及的 Angular 開發技巧，例如自定義 Angular Input 的 Typescript decorators、增強 ngTemplateOutlet 為 ngStringTemplateOutlet，Directive與Component的特別用法等等',
+      speaker: '謝亞東'
     },
     {
       isSession: false,
@@ -105,17 +107,23 @@ export const AgendaDataSet = {
       isSession: true,
       startTime: '14:30',
       endTime: '15:30',
-      title: 'TBD',
-      description: '',
-      speaker: 'TBD'
+      title: 'Missing Tutorial: Dive into Architecture for Angular ',
+      description: `
+          Even if you learn the basic usage of Angular, developing applications by yourself will be a lot of anxiety. The reason may be that there is not enough learning about the architecture of how to design an entire application.
+
+          Angular application is also a GUI application. Throughout the history of software development, many developers have devised best practices for the architecture of GUI applications. In other words, when thinking about the architecture of Angular applications, it is important to first learn from their history.
+
+          This presentation looks back on the common key principles underlying many architectural patterns, such as MVC, MVP, MVVM, or Clean Architecture. For example, the Single Responsibility Principle or Presentation Domain Separation. Then I'll introduce design patterns that will be a hint for applying those principles to Angular applications and considering an architecture that is highly compatible with the framework.`,
+      speaker: 'Suguru Inatomi '
     },
     {
       isSession: true,
       startTime: '15:30',
       endTime: '16:30',
-      title: 'TBD',
-      description: '',
-      speaker: 'TBD'
+      title: 'NX',
+      description: `
+      Angular 本身已是非常適合企業使用的架構，但面臨到超大型，數千個頁面的專案，若沒有更進一步的規劃開發流程、架構等細節，一樣會面臨到難以維護擴充等問題。而 Nx 的出現就是希望能更進一步將 Angular 架構得更加有條理，本次分享將分享講者在面臨到數千個頁面的專案時導入 Nx 架構的心得與成效！`,
+      speaker: 'Mike '
     },
     {
       isSession: true,
