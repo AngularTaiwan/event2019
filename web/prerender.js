60fa2e43c4afad3e61e44f6775e6937a5477413f
Kevin Yang <chgc_kevin@hotmail.com>
2019-08-07 14:48:43 +0800

    加入 prerender 功能

---
+++ web/prerender.js
@@ -0,0 +1,73 @@
+// npm install express puppeteer --save-dev
+// 設定路由 routes.json 檔
+
+const puppeteer = require('puppeteer');
+const express = require('express');
+const { join, dirname } = require('path');
+const { readFileSync, writeFileSync, mkdirSync, existsSync } = require('fs');
+
+function getJSON(fileName) {
+  if (!existsSync(fileName)) return {};
+  return JSON.parse(readFileSync(fileName));
+}
+
+const getDefaultProject = json => json.defaultProject || '';
+
+const getProjectOutput = json =>
+  json.projects[getDefaultProject(json)].architect.build.options.outputPath;
+
+const getProjectRoot = json => json.projects[getDefaultProject(json)].root;
+
+const PORT = 4000;
+const HOST = `http://localhost:${PORT}`;
+const angularJSON = getJSON('angular.json');
+
+async function main(routes) {
+  const projectOutputFolder = getProjectOutput(angularJSON);
+
+  const app = new express();
+  const index = (await readFileSync(
+    join(process.cwd(), projectOutputFolder, 'index.html')
+  )).toString();
+  app.get('*.*', express.static(join(process.cwd(), projectOutputFolder)));
+  app.get('*', (req, res) => res.send(index));
+
+  const server = await new Promise((resolve, reject) => {
+    const s = app.listen(PORT, e => (e ? reject(e) : resolve(s)));
+  });
+
+  console.log(`Started server ${HOST}!`);
+  const browser = await puppeteer.launch();
+  return Promise.all(
+    routes.map(async route => {
+      const page = await browser.newPage();
+      await page.goto(HOST + route);
+      const result = '<!DOCTYPE html>' + (await page.content());
+      // // Defining the html file name that will be created
+      const file = join(
+        process.cwd(),
+        projectOutputFolder,
+        route,
+        'index.html'
+      );
+
+      const dir = dirname(file);
+
+      // Test if the directory exist, if not create the directory
+      if (!(await existsSync(dir))) await mkdirSync(dir, { recursive: true });
+
+      // Write the rendered html file
+      await writeFileSync(file, result);
+    })
+  ).then(() => {
+    browser.close();
+    server.close();
+  });
+}
+
+const routesFile =
+  getProjectRoot(angularJSON) + '/' + (process.argv[2] || 'routes.json');
+
+Promise.resolve(getJSON(routesFile))
+  .then(async routes => await main(routes))
+  .then(() => console.log(`Finished Prerendering`));
