116a2b6258dd4768e82c4bbdda748eab23f3045b
Kevin Yang <chgc_kevin@hotmail.com>
2019-09-04 11:48:48 +0800

    調整議程時間

--- web/projects/event2019/src/app/agenda/AgendaDataSet.ts
+++ web/projects/event2019/src/app/agenda/AgendaDataSet.ts
@@ -69,44 +69,6 @@ export const AgendaDataSet = {
       isSession: true,
       startTime: '10:00',
       endTime: '11:00',
-      title: 'Lighthouse - Performance Tuning',
-      description: '',
-      speaker: 'Paul Li'
-    },
-    {
-      isSession: true,
-      startTime: '11:00',
-      endTime: '12:00',
-      title: 'Bazel',
-      description:
-        'Use Bazel to build your Angular Application - Bazel is an amazing build tool used in all products inside Google, and in the future version of Angular, bazel will also become the default build tool. Bazel is a fast/reliable/extensible build system with a lot of amazing features. In this talk we will introduce the basic concepts of bazel and also provide a demo to talk about how to build/test/bundle Angular application with bazel.',
-      speaker: '李嘉'
-    },
-    {
-      isSession: false,
-      startTime: '12:00',
-      endTime: '13:00',
-      title: 'Lunch Break 午餐時光'
-    },
-    {
-      isSession: true,
-      startTime: '13:00',
-      endTime: '14:00',
-      title: 'The hidden docs of Angular',
-      description:
-        '介紹一些有用但是 Angular 文檔上幾乎沒有涉及的 Angular 開發技巧，例如自定義 Angular Input 的 Typescript decorators、增強 ngTemplateOutlet 為 ngStringTemplateOutlet，Directive與Component的特別用法等等',
-      speaker: '謝亞東'
-    },
-    {
-      isSession: false,
-      startTime: '14:00',
-      endTime: '14:30',
-      title: 'Tea Break 下午茶時間 & Lighting Talk'
-    },
-    {
-      isSession: true,
-      startTime: '14:30',
-      endTime: '15:30',
       title: 'Missing Tutorial: Dive into Architecture for Angular ',
       description: `
           Even if you learn the basic usage of Angular, developing applications by yourself will be a lot of anxiety. The reason may be that there is not enough learning about the architecture of how to design an entire application.
@@ -118,20 +80,51 @@ export const AgendaDataSet = {
     },
     {
       isSession: true,
-      startTime: '15:30',
-      endTime: '16:30',
+      startTime: '11:00',
+      endTime: '12:00',
+      title: 'The hidden docs of Angular',
+      description:
+        '介紹一些有用但是 Angular 文檔上幾乎沒有涉及的 Angular 開發技巧，例如自定義 Angular Input 的 Typescript decorators、增強 ngTemplateOutlet 為 ngStringTemplateOutlet，Directive與Component的特別用法等等',
+      speaker: '謝亞東'
+    },
+    {
+      isSession: false,
+      startTime: '12:00',
+      endTime: '13:00',
+      title: 'Lunch Break 午餐時光'
+    },
+    {
+      isSession: true,
+      startTime: '13:00',
+      endTime: '14:00',
+      title: 'Bazel',
+      description:
+        'Use Bazel to build your Angular Application - Bazel is an amazing build tool used in all products inside Google, and in the future version of Angular, bazel will also become the default build tool. Bazel is a fast/reliable/extensible build system with a lot of amazing features. In this talk we will introduce the basic concepts of bazel and also provide a demo to talk about how to build/test/bundle Angular application with bazel.',
+      speaker: '李嘉'
+    },
+    {
+      isSession: false,
+      startTime: '14:00',
+      endTime: '15:00',
+      title: 'Tea Break 下午茶時間 & Lighting Talk'
+    },
+    {
+      isSession: true,
+      startTime: '15:00',
+      endTime: '16:00',
+      title: 'Lighthouse - Performance Tuning',
+      description:
+        '根據調查當人開一個網站等待時間超過 3 秒鐘時，會有很大的機率會直接離開，要怎麼知道自己的網站效能不錯，當然需要用工具來作評量，Google 的 Lighthouse 就是一個不錯的工具，除了提供檢測外，我們也可以透過檢測項目學習到開發網站的技巧，至於有什麼事情是我們一開始就可以留意的，就讓這議程告訴你',
+      speaker: 'Paul Li'
+    },
+    {
+      isSession: true,
+      startTime: '16:00',
+      endTime: '17:00',
       title: 'NX',
       description: `
       Angular 本身已是非常適合企業使用的架構，但面臨到超大型，數千個頁面的專案，若沒有更進一步的規劃開發流程、架構等細節，一樣會面臨到難以維護擴充等問題。而 Nx 的出現就是希望能更進一步將 Angular 架構得更加有條理，本次分享將分享講者在面臨到數千個頁面的專案時導入 Nx 架構的心得與成效！`,
       speaker: 'Mike '
-    },
-    {
-      isSession: true,
-      startTime: '16:30',
-      endTime: '17:00',
-      title: 'TBD',
-      description: '',
-      speaker: 'TBD'
     },
     {
       isSession: false,
